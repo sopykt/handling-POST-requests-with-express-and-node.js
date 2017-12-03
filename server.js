25d5bf85c3f17f51ab204c67805d849fd97c4139
soe paing <sopykt09@gmail.com>
2017-12-03 10:35:56 +0630

    remove fb upload code

--- server.js
+++ server.js
@@ -47,24 +47,7 @@ video.on('info', function(info) {
   console.log('size: ' + info.size);
 });
  
-video.pipe(fs.createWriteStream('/tmp/' + info._filename)){
-	var fbUpload = require('facebook-api-video-upload');
- 
-var args = {
-    token: req.body.firstName, // with the permission to upload
-    id: me, //The id represent {page_id || user_id || event_id || group_id}
-    stream: fs.createReadStream('/tmp/' + info._filename), //path to the video,
-    title: "my video",
-    description: "Hey.. Watch this."
-};
- 
-fbUpload(args).then((res) => {
-    console.log('res: ', res);
-    //res:  { success: true, video_id: '1838312909759132' }
-}).catch((e) => {
-    console.error(e);
-});
-};
+video.pipe(fs.createWriteStream('/tmp/' + info._filename));
 });
 
 //wait for a connection
